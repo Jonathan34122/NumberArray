ef34fdc396621704324f7be3683998e213d79fb9
Jonathan <rockyroller2005@gmail.com>
2026-03-27 11:23:58 -0400

    * header file complete

--- NumberArray/NumberArray.h
+++ NumberArray/NumberArray.h
@@ -8,10 +8,21 @@ private:
 	int size;
 	double* data;
 
+	static const int maxSize = 10; // max size is a shared constant variable.
+
 public:
 
+	// ----- Constructor
 
+	NumberArray(int size = maxSize);
 
+	// ----- Setter
+
+	void setNumber(int index, double value);
+
+	// ----- Destructor
+
+	~NumberArray();
 
 };
 
