73997b6cca48c801ea9729c8752027e8e8832080
Jonathan <rockyroller2005@gmail.com>
2026-05-07 17:19:47 -0400

    *constructor, copy constructor and assigment op defined.

--- NumberArray/NumberArray.h
+++ NumberArray/NumberArray.h
@@ -1,6 +1,11 @@
 #ifndef NUMBERARRAY_H
 #define NUMBERARRAY_H
 
+#include <iostream>
+#include <stdexcept>
+
+
+template <typename T>
 class NumberArray {
 
 private:
@@ -14,12 +19,71 @@ public:
 
 	// ----- Constructor
 
-	NumberArray(int size = maxSize);
+	NumberArray(int size = maxSize) {
 
-	// = Op Overload Initialize
+		if (size <= 0 || size > maxSize) {
 
-	NumberArray(const NumberArray& other);
-	NumberArray& operator=(const NumberArray& other);
+			this->size = maxSize;
+
+		}
+		else {
+			this->size = size;
+		}
+
+		data = new T[this->size];
+
+		for (int i = 0; i < this->size; i++) {
+
+			data[i] = 0;
+
+		}
+
+		cout << "Constructor Called";
+
+	}
+
+	// ----- Copy Constructor
+
+	NumberArray(const NumberArray<T>& other) {
+
+		size = other.size;
+		data = new T[size];
+
+		for (int i = 0; i < size; i++) {
+
+			data[i] = other.data[i];
+
+		}
+
+		cout << "Copy Constructor Called"
+
+	}
+
+	// ----- Assignment Op
+
+	NumberArray<T>& operator=(const NumberArray& other) {
+
+		if (this == &other) {
+
+			return *this;
+
+		}
+
+		delete[] data;
+		size = other.size;
+		data = new T[size];
+
+		for (int i = 0; i < size; i++) {
+
+			data[i] = other.data[i];
+
+		}
+
+		cout << "Assignment Operator Called";
+
+		return *this;
+
+	}
 
 
 	// ----- Setter
